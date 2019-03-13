411f5ab0c0ca586bbfde83ad48143fa1f6e3abf8
TrandeLik <feoctistov1580@yandex.ru>
2019-03-13 14:00:41 +0300

    one more patch

--- Проект 1. Резюме/Big(Yellow)RubberDuck/fonts.js
+++ Проект 1. Резюме/Big(Yellow)RubberDuck/fonts.js
@@ -2,8 +2,9 @@ var btn = document.getElementById("submit");
 var body= document.getElementById("body");
 
 btn.onclick = function () {
-        var selind = document.getElementById("font").options.selectedIndex;
-        console.log(selind);
-        body.style.fontFamily = document.getElementById("font").options[selind].text;
-
+        let i = document.getElementById("font").options.selectedIndex;
+        body.style.fontFamily = document.getElementById("font").options[i].text;
+        i = document.getElementById("size").options.selectedIndex;
+        console.log(i);
+        body.style.fontSize = document.getElementById("size").options[i].text +'px';
 };
